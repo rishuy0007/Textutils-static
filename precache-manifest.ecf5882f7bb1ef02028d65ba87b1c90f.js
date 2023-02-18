63e03f505e68c2d67184429214cd547da684a4fb
Rishabh Yadav <rishuy0007@gmail.com>
2023-02-18 19:00:53 +0530

    Updates

---
+++ precache-manifest.ecf5882f7bb1ef02028d65ba87b1c90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96d8f77088ad655bd398",
+    "url": "/Textutils-static/static/css/main.db2577a9.chunk.css"
+  },
+  {
+    "revision": "96d8f77088ad655bd398",
+    "url": "/Textutils-static/static/js/main.ed6ce4aa.chunk.js"
+  },
+  {
+    "revision": "bfe7c97f803e6eed6d5c",
+    "url": "/Textutils-static/static/js/runtime~main.29e3d150.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/Textutils-static/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/Textutils-static/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "0925a7965fc8e55cb55392a0ba0bf395",
+    "url": "/Textutils-static/index.html"
+  }
+];
